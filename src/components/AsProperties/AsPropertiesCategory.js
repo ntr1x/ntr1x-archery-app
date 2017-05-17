b832f9b0b51dda079fce3768d19e8502ab461f86
Igor Pavlenko <ipavlenko@inbox.ru>
2017-05-18 00:21:57 +0500

    Shell & Properties view

--- src/components/AsProperties/AsPropertiesCategory.js
+++ src/components/AsProperties/AsPropertiesCategory.js
@@ -2,5 +2,13 @@ export default {
   name: 'as-properties-category',
   props: {
     title: String
+  },
+  data: () => ({
+    open: true
+  }),
+  methods: {
+    toggle () {
+      this.open = !this.open
+    }
   }
 }
