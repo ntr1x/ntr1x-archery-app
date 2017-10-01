4dd1272cc1831eb43916ffcbcebc0d68c70ce155
Igor Pavlenko <ipavlenko@inbox.ru>
2017-10-01 20:30:37 +0500

    Better scroll

--- src/components/shell/Toolbar/Toolbar.js
+++ src/components/shell/Toolbar/Toolbar.js
@@ -6,7 +6,7 @@ export default {
       pc: { name: 'pc', position: 'absolute', width: '100%', height: '100%' },
       tablet: { name: 'tablet', position: 'relative', width: '768px', height: '1024px' },
       phone: { name: 'phone', position: 'relative', width: '414px', height: '736px' },
-      custom: { name: 'custom', position: 'relative', width: '1280px', height: '780px' }
+      custom: { name: 'custom', position: 'relative', width: '1280px', height: '100%' }
     }),
     ...mapState({
       active: state => state.designer.dimensions,
